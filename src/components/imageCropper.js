e574e922ef001f418d125d6333b93db44ef5a61b
Chang Chen <changdreamdev@gmail.com>
2024-03-24 05:15:30 -0700

    fix bug

--- src/components/imageCropper.js
+++ src/components/imageCropper.js
@@ -24,7 +24,7 @@ const ImageCropper = (props) => {
   const [completedCrop, setCompletedCrop] = useState();
   const [imageLoadFlag, setImageLoadFlag] = useState(false);
 
-  const onImageLoaded = (e) => {
+  const onImageLoaded = async (e) => {
     // Do something when the image is loaded
     // onCropComplete({
     //   ...crop,
@@ -34,7 +34,11 @@ const ImageCropper = (props) => {
     //   height: image.target.height,
     // });
     // eslint-disable-next-line
+
     setImageLoadFlag(true);
+    await delayFunc();
+    const { width, height } = imageRef.current;
+    makeClientCrop(convertToPixelCrop(crop, width, height));
   };
 
   const onCropChange = (newCrop, percentCrop) => {
